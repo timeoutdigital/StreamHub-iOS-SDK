508339af231349b100dd63219d2c52ab30aa09b8
kvanadev <kvanadev1@gmail.com>
2016-06-15 16:52:03 +0530

    Removed copyright

--- LFSClient/Classes/LFHUD.h
+++ LFSClient/Classes/LFHUD.h
@@ -1,10 +1,3 @@
-//
-//  MumsyHUD.h
-//  Mumsy
-//
-//  Created by Kvana Inc 1 on 06/05/15.
-//  Copyright (c) 2015 Kvana. All rights reserved.
-//
 
 #import <Foundation/Foundation.h>
 #import <MBProgressHUD/MBProgressHUD.h>
